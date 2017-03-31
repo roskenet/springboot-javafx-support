14357cfe1b81dc7af6f59e6b8c33a9edca23eecd
Felix Roske <f.roske@roskenet.de>
2017-03-31 22:26:30 +0200

    Add progressbar to splash screen

--- src/main/java/de/felixroske/jfxsupport/SplashScreen.java
+++ src/main/java/de/felixroske/jfxsupport/SplashScreen.java
@@ -1,9 +1,11 @@
 package de.felixroske.jfxsupport;
 
 import javafx.scene.Parent;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /*
  * A default standard splash pane implementation Subclass it and override it's methods to customize with your own
@@ -18,10 +20,14 @@ public class SplashScreen {
      * @return A standard image
      */
     public Parent getParent() {
-        Pane imagePane = new Pane();
-        Image img = new Image(getClass().getResource(getImagePath()).toExternalForm());
-        imagePane.getChildren().add(new ImageView(img));
-        return imagePane;
+        ImageView imageView = new ImageView(getClass().getResource(getImagePath()).toExternalForm());
+        ProgressBar splashProgressBar = new ProgressBar();
+        splashProgressBar.setPrefWidth(imageView.getImage().getWidth());
+        
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(imageView, splashProgressBar);
+        
+        return vbox;
     }
 
     /**
